2b091d069cf6ca9eb5ce034d0d7226e851fec043
Leif Peterson <contact@lmp3d.com>
2016-01-24 19:37:39 -0800

    Transitioned to PyMEL and added vertex clean up

--- DelRandIntEdges.py
+++ DelRandIntEdges.py
@@ -1,24 +1,51 @@
-# This code can be used for deleting random interior edges on a Maya PolyObject
-import maya.cmds as cmds
+# DelRandIntEdges.py
+#
+# Leif Peterson 2016
+#
+# This code deletes random interior edges and cleans up winged vertices, while maintaing corners
+
+import pymel.core as pm
 import random
 
-# Get the Current Selection
-selObjs = cmds.ls( sl = True ) 
+# create a list of all selected objects
+SelectedObjects = pm.ls( selection=True, o=True )
 
-# For each object in the selection
-for Obj in range(len(selObjs)):
-    # Select only the interior edges
-    cmds.selectType( pe=True )
-    cmds.polySelectConstraint( mode=3, type=0x8000, where=2 )
+# for each object delete its interior edges randomly 
+for Object in range(len(SelectedObjects)):
+    # select only border edges
+    pm.selectType( pe=True )
+    pm.hilite( SelectedObjects[Object] )
+    pm.polySelectConstraint( mode=3, type=0x8000, where=1 )
+    pm.select()
+    borderEdges = pm.ls( selection=True, fl=True )
     
-    # List the selected edges
-    iEdges = cmds.ls( selection=True, fl=True )
+    # select all edges
+    pm.polySelectConstraint( where=0 )
+    pm.select()
+    allEdges = pm.ls( selection=True, fl=True )
     
-    # For each edge in the list, delete it if the random int is greater than 5
-    for edge in range(len(iEdges)):
+    # initilize the inside edges list
+    insideEdges = []
+    
+    # remove border edges
+    for edge in range(len(borderEdges)):
+        allEdges.remove( borderEdges[edge] )
+    
+    # create a list of interior edges from the list of all edges with the border edges removed    
+    for edge in range(len(allEdges)):
+        insideEdges.append(allEdges[edge])
+    
+    # clear selection
+    pm.select( cl=True )  
+    
+    # randomly select and delete an interior edge   
+    for iEdge in range(len(insideEdges)):
         if random.randrange( 0, 9 ) > 5:
-            #print iEdges[edge]
-            cmds.delete( iEdges[edge] )
-            
-    # Release the Selection Constraint when finished       
-    cmds.polySelectConstraint( where=0 )
+            pm.select( insideEdges[iEdge] )
+            pm.polyDelEdge( cv=False )
+    
+    # Clean up the winged vertices that are along straight edges, preserving corners        
+    pm.selectType( pv=True )
+    pm.polySelectConstraint( angle=1, type=0x0001, mode=3, anglebound=[ 91.0, 181.0 ] )
+    pm.select()
+    pm.polyDelVertex()
