20c8c5721b27c8932107d0b2024458012e39e963
Leif Peterson <contact@lmp3d.com>
2016-01-26 20:24:22 -0800

    Update sdl2-in-maya.py

--- sdl2-in-maya.py
+++ sdl2-in-maya.py
@@ -1,15 +1,31 @@
+#import pymel.core as pm
 import os
 import sys
 import ctypes
 import sdl2
+import sdl2.ext
 
-def run():
-    sdl2.SDL_Init(sdl2.SDL_INIT_TIMER|sdl2.SDL_INIT_GAMECONTROLLER|sdl2.SDL_INIT_EVENTS)
+def init():
+    
+    if sdl2.SDL_Init(sdl2.SDL_INIT_TIMER|sdl2.SDL_INIT_GAMECONTROLLER|sdl2.SDL_INIT_EVENTS) != 0:
+        print(sdl2.SDL_GetError())
+        return -1
+        
+    ctrlWindow = sdl2.SDL_CreateWindow( b"Control", sdl2.SDL_WINDOWPOS_UNDEFINED, sdl2.SDL_WINDOWPOS_UNDEFINED, 200, 150, sdl2.SDL_WINDOW_INPUT_FOCUS )
+    if not ctrlWindow:
+        print(sdl2.SDL_GetError())
+        return -1
+    #sdl2.SDL_Delay(10)    
+    sdl2.SDL_DestroyWindow(ctrlWindow)
+    sdl2.SDL_Quit()
+    return ctrlWindow
+        
+#ticks = sdl2.SDL_GetTicks()
+#print ticks
+#sdl2.SDL_WINDOW_HIDDEN
 
-ticks = sdl2.SDL_GetTicks()
+init()
 
-print ticks
-
-run()
+sdl2.SDL_DestroyWindow(ctrlWindow)
 
 sdl2.SDL_Quit()
