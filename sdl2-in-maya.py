a66e6b9eabdd5c4e01884713ec13b4468c644bb7
Leif Peterson <contact@lmp3d.com>
2016-01-27 01:36:16 -0800

    Update sdl2-in-maya.py

--- sdl2-in-maya.py
+++ sdl2-in-maya.py
@@ -2,30 +2,57 @@
 import os
 import sys
 import ctypes
+from ctypes import *
+import collections
 import sdl2
 import sdl2.ext
 
-def init():
+# define structures to store 2D and 3D floating point values
+class Vector3(Structure):
+    _fields_=[("x", c_float), ("y", c_float), ("z", c_float)]
+    
+class Vector2(Structure):
+    _fields_=[("x", c_float), ("y", c_float)]
+    
+    
+
+def run():
+    
+    MovementVector = Vector2( 0.0, 0.0 )
     
     if sdl2.SDL_Init(sdl2.SDL_INIT_TIMER|sdl2.SDL_INIT_GAMECONTROLLER|sdl2.SDL_INIT_EVENTS) != 0:
         print(sdl2.SDL_GetError())
         return -1
         
-    ctrlWindow = sdl2.SDL_CreateWindow( b"Control", sdl2.SDL_WINDOWPOS_UNDEFINED, sdl2.SDL_WINDOWPOS_UNDEFINED, 200, 150, sdl2.SDL_WINDOW_INPUT_FOCUS )
+    ctrlWindow = sdl2.SDL_CreateWindow( b"Control", sdl2.SDL_WINDOWPOS_UNDEFINED, sdl2.SDL_WINDOWPOS_UNDEFINED, 200, 150, sdl2.SDL_WINDOW_INPUT_FOCUS|sdl2.SDL_WINDOW_HIDDEN )
     if not ctrlWindow:
-        print(sdl2.SDL_GetError())
+        print(sdl2.GetError())
         return -1
-    #sdl2.SDL_Delay(10)    
+    
+    running = True    
+    while running:
+        evnt = sdl2.SDL_Event()
+        while sdl2.SDL_PollEvent(ctypes.byref(evnt)) != 0:
+            if evnt.type == sdl2.SDL_QUIT:
+                running = False
+                break
+            elif evnt.type == sdl2.SDL_CONTROLLERAXISMOTION:
+                MovementVector.x = float(evnt.motion.x)
+                MovementVector.y = float(evnt.motion.y)
+                break
+        print MovementVector.x, MovementVector.y
+        sdl2.SDL_Delay(10)
+         
+            
     sdl2.SDL_DestroyWindow(ctrlWindow)
     sdl2.SDL_Quit()
-    return ctrlWindow
+    return 0
         
 #ticks = sdl2.SDL_GetTicks()
 #print ticks
 #sdl2.SDL_WINDOW_HIDDEN
 
-init()
+run()
 
-sdl2.SDL_DestroyWindow(ctrlWindow)
 
 sdl2.SDL_Quit()
