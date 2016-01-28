a4090cc62a270c8cbaa2eaa79f574d2a0f6ff793
Leif Peterson <contact@lmp3d.com>
2016-01-27 21:55:37 -0800

    Update sdl2-in-maya.py

--- sdl2-in-maya.py
+++ sdl2-in-maya.py
@@ -1,4 +1,4 @@
-#import pymel.core as pm
+import pymel.core as pm
 import os
 import sys
 import ctypes
@@ -20,13 +20,26 @@ class IntVector2(Structure):
 class FlVector2(Structure):
     _fields_=[("x", c_float), ("y", c_float)]    
 
+# Pawn class - has an object and can move
+#class Pawn:
+    #def __init__(self, speed, mayaObj):
+        #fSpeed = speed
+        #rColor = 1
+        #gColor = 0
+        #bColor = 0
+        #aColor = 1
+        #mObj = mayaObj
+        
+    #def update(self):
+        #pm.xform( mObj[0], r=True, wd=True, t=[ 1.0, 0.0, 1.0] )
+
 def RemapMovement( RawMoveX, RawMoveY ):
     NormX = 0.0
     NormY = 0.0
     NormalMove = FlVector2( NormX, NormY )
     RawX = RawMoveX
     RawY = RawMoveY
-    # Dead Zone is between -4500 and 4500
+    # Dead Zone is between -6000 and 6000
     # Convert the integer a value between -1 and 1
     if abs(RawX) < 6000 :
         NormX = 0.0
@@ -64,6 +77,15 @@ def run():
         print(sdl2.GetError())
         return -1
     
+    # Init Player
+    player = pm.polyCube( name="Player", w=1, h=1, d=1, cuv=0, ax=(0,1,0), sx=1, sy=1, sz=1 )
+    if player is None:
+        print "ERROR! Player not created!"
+        return -1
+    else:
+        pm.polyColorPerVertex( player, colorRGB=(1.0,0.0,0.0), alpha=1.0, notUndoable=True )
+    
+    
     # Start Game Loop
     running = True    
     while running:
@@ -94,6 +116,7 @@ def run():
         # Remap Raw Movement to -1 to 1 in float
         Movement = RemapMovement( RawMovementVector.x, RawMovementVector.y )
         print Movement.x, Movement.y
+        pm.xform( player, r=True, wd=True, t=[ Movement.x, 0.0, Movement.y ] )
         sdl2.SDL_Delay(15)      
             
     sdl2.SDL_DestroyWindow(ctrlWindow)
