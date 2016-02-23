063da572cc9155c114b8edf0ecfc74a357c371f4
Leif Peterson <contact@lmp3d.com>
2016-02-23 12:08:24 -0800

    Create VertexColorGreyNoise.py

---
+++ VertexColorGreyNoise.py
@@ -0,0 +1,9 @@
+## VertexColorGreyNoise.py ##
+##     Version 1.0     ##
+## Leif Peterson  2016 ##
+
+## This Script Creates a Second Vertex Color Set on the Selected Object named "colorSet2"
+## and adds a Noise Gradient on which the user can select a Minimum and Maximum Value.
+
+# Import Modules
+import pymel.core as pm
