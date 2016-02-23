97bd3d2b6112b647c70592f4f4af2d29352ec24a
Leif Peterson <contact@lmp3d.com>
2016-02-23 14:07:56 -0800

    Update VertexColorGreyNoise.py

--- VertexColorGreyNoise.py
+++ VertexColorGreyNoise.py
@@ -7,3 +7,47 @@
 
 # Import Modules
 import pymel.core as pm
+import random
+
+
+
+# Check if Object has Multiple Vertex Color Sets
+def HasMultipleColorSets():
+    ColorSets = pm.polyColorSet( query=True, allColorSets=True )
+    if len(ColorSets) > 1:
+        return True
+    elif len(ColorSets) == 1:
+        return False
+        
+# Create Second Vertex Color Set
+def CreateSecondColorSet( SelectedObject ):
+    if HasMultipleColorSets() == True:
+        return 0
+        
+        
+#selected = pm.ls( selection=True )        
+#print len(selected)       
+        
+# Primary Function
+def Main():
+    # Check that a Valid Selection has been made
+    Selected = pm.ls( selection = True ) 
+    if len(Selected) == 0:
+        return "No Objects Selected"
+    elif len(Selected) > 1:
+        return "More Than One Object Selected"
+    # If there is a Valid Selection, Execute the script
+    elif len(Selected) == 1:
+        # Get the Selection's Shape
+        SelectionShape = Selected[0].getShape()
+        # Check if the Object Type is a Polygon Mesh
+        IsValidObjType = pm.objectType( SelectionShape, isType='mesh' )
+        # If it is Not, Exit the Function and tell the User
+        if IsValidObjType != True:
+            return "Selected Object is Not a Polygon Mesh"
+        else:
+            print "is valid"
+        
+        
+    else:
+        return "Unknown Error Occurred"
